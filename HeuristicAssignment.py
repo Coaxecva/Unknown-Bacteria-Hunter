df455898e93076690559e6cda06acd25b1fd1a74
Quang Tran <tmquang@gmail.com>
2016-09-23 13:33:52 -0500

    Update HeuristicAssignment.py

--- HeuristicAssignment.py
+++ HeuristicAssignment.py
@@ -1,27 +1,11 @@
 import sys, os
 
-fn1 = sys.argv[1]
-f1 = open(fn1)
+fname = sys.argv[1]
 
-fs = True
-idx = 0
-#t = set()
+with open(fname) as f:
+    content = f.readlines()
 
-while True:
-	info= f1.readline()
-	if info=='' or info==" ":
-		break
-	if info[0]=="#":
-		idx += 1		
-		while f1.readline()[0] == "#": continue
-		while True:
-			info = f1.readline()
-			if not info.split() or info[0]=="#":
-				break
-			print(info.split())
-			while f1.readline()[0]!="#" or f1.readline()[0]!='' or f1.readline()[0]!=" ": continue
-			break
-		
-print(idx)
+for i in xrange(len(rcontent)):
+	if line[i].startswith("#") and not line[i+1].startswith("#"):
+		print(line[i+1])
 
-f1.close()
