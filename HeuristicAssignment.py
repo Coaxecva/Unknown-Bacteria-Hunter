6f94fc154a70d991cb48ca81f4414f6567b1f39f
Quang Tran <tmquang@gmail.com>
2016-09-23 13:51:24 -0500

    Update HeuristicAssignment.py

--- HeuristicAssignment.py
+++ HeuristicAssignment.py
@@ -1,11 +1,31 @@
 import sys, os
+from Bio import SeqIO
 
 fname = sys.argv[1]
+fseq = sys.argv[2]
 
 with open(fname) as f:
     content = f.readlines()
 
-for i in xrange(len(rcontent)):
-	if line[i].startswith("#") and not line[i+1].startswith("#"):
-		print(line[i+1])
+#print(len(content))
+
+count = 0
+
+# Extract significant names
+for i in range(len(content)):
+	if content[i].startswith("#") and not content[i+1].startswith("#"):
+		print(content[i+1].rstrip())
+		count += 1
+
+print("# of seqs: ", count)
+
+
+
+handle = open(fseq, "rU")
+print(fseq)
+for record in SeqIO.parse(handle, "fasta"):
+    print(record.id)
+handle.close()
+
+
 
