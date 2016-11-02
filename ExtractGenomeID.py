b7a20e6a673ee5c7bec3e3199b1c0762d155501c
Quang Tran <tmquang@gmail.com>
2016-11-02 10:57:28 -0500

    Update ExtractGenomeID.py

--- ExtractGenomeID.py
+++ ExtractGenomeID.py
@@ -21,13 +21,23 @@ def RetriveGenome(fname):
 	with open(fname) as f:
 		content = [line.rstrip() for line in f]
 		
+	# Open a file
+	fo1 = open("correct_reads.txt", "wb")
+	fo2 = open("correct_clusters.txt", "wb")
+
+	
 	for i, read in enumerate(content):
 		if read in GenomeID:
-			print(GenomeID[read])
+			fo1.write(read);
+			fo2.write(GenomeID[read]);
+		#	print(GenomeID[read])
 		# else:
 		# 	seq = Seq(read)
 		# 	print(i, read, str(seq.reverse_complement()), str(seq.reverse_complement()) in GenomeID)
 
+	# Close opend file
+	fo1.close()
+	fo2.close()
 
 if __name__ == '__main__':
 
