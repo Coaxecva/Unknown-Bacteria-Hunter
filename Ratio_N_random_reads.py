917a2900edb5e069b86be507086c3f74926624c1
Quang Tran <tmquang@gmail.com>
2017-01-20 12:57:33 -0600

    Update Ratio_N_random_reads.py

--- Ratio_N_random_reads.py
+++ Ratio_N_random_reads.py
@@ -9,11 +9,22 @@ def ShuffleArray(fname):
 	random.shuffle(content)
 	return content
 
+def ShuffleArray1(fname, c):
+	# fname of corrected clusters
+	num_lines = sum(1 for line in open(fname))
+	#print(num_lines)
+	print(num_lines/c)
+
 if __name__ == '__main__':
 
-	read_f = sys.argv[1]
-	arr = ShuffleArray(read_f)
+	#read_f = sys.argv[1]
+	#arr = ShuffleArray(read_f)
 	#print(arr)
 	#print(len(arr))
+
+	fname = sys.argv[1]
+	num_unknown = sys.argv[2]
+	ShuffleArray1(fname, int(num_unknown))
+
 
 	
