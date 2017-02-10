ff93398096ec5040b447a127aedf92a86e44aca9
Quang Tran <tmquang@gmail.com>
2017-02-10 15:28:52 -0600

    Create Ratio_N_random_reads.py

---
+++ Optimize parameters/code/Ratio_N_random_reads.py
@@ -0,0 +1,101 @@
+#python Ratio_N_random_reads.py ../Data/test-data/ds_20170124/1pct/ds3/correct_clusters_c40.txt 9
+
+import random, sys, string
+import pandas as pd
+import math
+from collections import Counter
+import numpy as np
+
+from sklearn.metrics.cluster import adjusted_mutual_info_score, adjusted_rand_score, homogeneity_score, completeness_score
+from sklearn.metrics import jaccard_similarity_score
+
+def ShuffleArray(fname):
+	with open(fname) as f:
+		content = [read.strip() for read in f]
+
+	#print(content)
+	#print(len(content))
+	random.shuffle(content)
+	return content
+
+def ShuffleArray1(fname, c):
+	# fname of corrected clusters
+	num_lines = sum(1 for line in open(fname))
+	#print(num_lines)
+	#print(num_lines/c)
+	arr = []
+
+	for i in range(c):
+		for j in range(num_lines//c):
+			arr.append(i)	
+
+	for j in range(num_lines - len(arr)):
+		arr.append(i)
+
+	# shuffle labels
+	random.shuffle(arr)
+	return arr
+
+def eta(data, unit='natural'):
+	base = { 'shannon' : 2.,
+			'natural' : math.exp(1),
+			'hartley' : 10. }
+	if len(data) < 1:
+		return 0, -1
+
+	counts = Counter()
+	for d in data:
+		counts[d] += 1
+
+	probs = [float(c) / len(data) for c in counts.values()]
+	probs = [p for p in probs if p > 0.]
+	ent = 0
+
+	for p in probs:
+		if p > 0.:
+			ent -= p * math.log(p, base[unit])
+	
+	return ent, counts
+
+
+if __name__ == '__main__':
+
+	#read_f = sys.argv[1]
+	#arr = ShuffleArray(read_f)
+	#print(arr)
+	#print(len(arr))
+
+	fname = sys.argv[1]  # correct clusters
+	num_unknown = sys.argv[2]
+
+	# shuffle array
+	arr = ShuffleArray1(fname, int(num_unknown))
+	#print(arr)
+	
+	with open(fname) as f:
+		content = [line.rstrip() for line in f]
+
+	print(adjusted_mutual_info_score(content, arr), "\t", adjusted_rand_score(content, arr),
+			"\t", homogeneity_score(content, arr), "\t", completeness_score(content, arr),
+			"\t", jaccard_similarity_score(content, arr))
+	# Compute entropy
+	unique_val = set(arr)
+	#print(len(unique_val))
+
+	df = pd.DataFrame({ 'groundtruth': content, 
+							'predict': arr })
+
+	en_arr = []
+	for label in unique_val:
+		#print(label)
+		#print(df[df['predict']==label])
+		group = list(df[df['predict']==label]['groundtruth'])
+		en, ok = eta(group, "shannon")
+		if ok != -1:
+			print(en)
+			en_arr.append(en)
+			#for key, value in ok.items():
+			#	print("\t", key, value)
+
+	print(np.average(en_arr))
+	print(np.median(en_arr))
