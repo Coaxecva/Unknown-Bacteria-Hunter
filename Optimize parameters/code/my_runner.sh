de4eaef209e7013f241b165cadb1fcae86b6c3e9
Quang Tran <tmquang@gmail.com>
2017-02-15 19:30:56 -0600

    Update my_runner.sh

--- Optimize parameters/code/my_runner.sh
+++ Optimize parameters/code/my_runner.sh
@@ -1,20 +1,85 @@
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.3 > pc_c30_4_05.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.4 > pc_c30_4_04.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.5 > pc_c30_4_05.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.6 > pc_c30_4_06.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.7 > pc_c30_4_07.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.8 > pc_c30_4_08.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.3 > pc_c30_4_05.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.4 > pc_c30_4_04.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.5 > pc_c30_4_05.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.6 > pc_c30_4_06.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.7 > pc_c30_4_07.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.8 > pc_c30_4_08.txt &
 
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.3 > pc_c35_2_0.3.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.4 > pc_c35_2_0.4.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.5 > pc_c35_2_0.5.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.6 > pc_c35_2_0.6.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.7 > pc_c35_2_0.7.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.8 > pc_c35_2_0.8.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.3 > pc_c35_2_0.3.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.4 > pc_c35_2_0.4.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.5 > pc_c35_2_0.5.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.6 > pc_c35_2_0.6.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.7 > pc_c35_2_0.7.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.8 > pc_c35_2_0.8.txt &
 
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.3 > pc_c40_2_0.3.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.4 > pc_c40_2_0.4.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.5 > pc_c40_2_0.5.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.6 > pc_c40_2_0.6.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.7 > pc_c40_2_0.7.txt &
-go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.8 > pc_c40_2_0.8.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.3 > pc_c40_2_0.3.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.4 > pc_c40_2_0.4.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.5 > pc_c40_2_0.5.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.6 > pc_c40_2_0.6.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.7 > pc_c40_2_0.7.txt &
+#go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.8 > pc_c40_2_0.8.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.85 > pc_c30_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c30.txt -sr 4 -ot 0.9 > pc_c30_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.85 > pc_c35_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c35.txt -sr 4 -ot 0.9 > pc_c35_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.85 > pc_c40_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c40.txt -sr 4 -ot 0.9 > pc_c40_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.5 > pc_c10_2_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.6 > pc_c10_2_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.7 > pc_c10_2_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.8 > pc_c10_2_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.85 > pc_c10_2_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c10.txt -sr 4 -ot 0.9 > pc_c10_2_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.5 > pc_c20_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.6 > pc_c20_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.7 > pc_c20_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.8 > pc_c20_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.85 > pc_c20_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c20.txt -sr 4 -ot 0.9 > pc_c20_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.5 > pc_c50_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.6 > pc_c50_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.7 > pc_c50_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.8 > pc_c50_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.85 > pc_c50_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c50.txt -sr 4 -ot 0.9 > pc_c50_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.5 > pc_c60_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.6 > pc_c60_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.7 > pc_c60_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.8 > pc_c60_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.85 > pc_c60_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c60.txt -sr 4 -ot 0.9 > pc_c60_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.5 > pc_c70_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.6 > pc_c70_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.7 > pc_c70_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.8 > pc_c70_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.85 > pc_c70_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c70.txt -sr 4 -ot 0.9 > pc_c70_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.5 > pc_c80_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.6 > pc_c80_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.7 > pc_c80_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.8 > pc_c80_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.85 > pc_c80_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c80.txt -sr 4 -ot 0.9 > pc_c80_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.5 > pc_c90_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.6 > pc_c90_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.7 > pc_c90_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.8 > pc_c90_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.85 > pc_c90_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c90.txt -sr 4 -ot 0.9 > pc_c90_4_0.9.txt &
+
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.5 > pc_c100_4_0.5.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.6 > pc_c100_4_0.6.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.7 > pc_c100_4_0.7.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.8 > pc_c100_4_0.8.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.85 > pc_c100_4_0.85.txt &
+go run ~/Documents/Metagenomics/src/OverlapComponent.go -qf correct_reads_c100.txt -sr 4 -ot 0.9 > pc_c100_4_0.9.txt &
